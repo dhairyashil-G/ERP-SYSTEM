cd6323c5ee562a27969ac587eefd4578ac60182c
Dhairyashil <dhairyasheel.ghatage@gmail.com>
2023-09-10 17:36:07 +0530

    calculations fixed

--- erp_sys/RawProducts/models.py
+++ erp_sys/RawProducts/models.py
@@ -20,3 +20,24 @@ class RawProduct(models.Model):
 
     fcode = models.CharField(max_length=20, default="N.A")
     # Add more fields as per your requirements
+
+
+class FinalRawProduct(models.Model):
+    name = models.CharField(max_length=100)
+
+    final_calcium_content = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
+    final_magnesium_content = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
+    final_zinc_content = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
+    final_nitrogen_content = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
+    final_moly_content = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
+    
+    final_TBN_content = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
+    final_nitrogen_content = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
+    final_phosphorus_content = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
+    final_sulfur_content = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
+    final_boron_content = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
+
+
+    final_quantity = models.IntegerField()
+
+    final_fcode = models.CharField(max_length=20, default="N.A")
