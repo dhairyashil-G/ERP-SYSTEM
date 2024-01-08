1cd659d240bfc9278de9681e0124cdb4405ceeef
Dhairyashil Anil Ghatage <dhairyasheel.ghatage@gmail.com>
2024-01-08 19:31:49 +0530

    updated database

--- frontend/src/pages/RawProductsPage.jsx
+++ frontend/src/pages/RawProductsPage.jsx
@@ -1,34 +1,45 @@
 import React from "react";
-import Heading from "../components/extras/Heading"; 
+import Heading from "../components/extras/Heading";
 import { Link } from "react-router-dom";
 
 // RawProductPage.js
 function RawProductPage() {
+  const cards = [
+    {
+      title: "List Raw Products",
+      link: "/rawproducts/list",
+      description: "View a list of raw products.",
+    },
+    {
+      title: "Create Raw Product",
+      link: "/rawproducts/create",
+      description: "Create a new raw product.",
+    },
+    {
+      title: "Update Raw Products",
+      link: "/rawproducts/update",
+      description: "Update existing raw products.",
+    },
+  ];
+
   return (
-    <div>
-      <Heading heading='Raw Product Page'></Heading>
-      <div>
-        <Link to="/rawproducts/list">
-          <button className="mx-4 my-3 w-2/12 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-            List Raw Products
-          </button>
-        </Link>
+    <>
+      <div className="mx-auto mb-4">
+        <Heading heading="Raw Product Page"></Heading>
       </div>
-      <div>
-        <Link to="/rawproducts/create">
-          <button className="mx-4 my-3 w-2/12 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-            Create Raw Product
-          </button>
-        </Link>
+      <div className="flex flex-row justify-center space-x-6">
+        {cards.map((card, index) => (
+          <Link key={index} to={card.link}>
+            <div className="max-w-sm rounded mx-9 overflow-hidden shadow-lg bg-white border border-gray-300 transition transform hover:scale-105">
+              <div className="px-6 py-4">
+                <div className="font-bold text-xl mb-2">{card.title}</div>
+                <p className="text-gray-700 text-base">{card.description}</p>
+              </div>
+            </div>
+          </Link>
+        ))}
       </div>
-      <div>
-        <Link to="/rawproducts/update">
-          <button className="mx-4 my-3 w-2/12 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-            Update Raw Products
-          </button>
-        </Link>
-      </div>
-    </div>
+    </>
   );
 }
 
